3ab03d26713dc7d2087fce86a2cb3db6a4ffd87f
knvcodes <knvcodes@gmail.com>
2026-06-29 23:12:00 +0530

    fix: try new cors

--- src/server.ts
+++ src/server.ts
@@ -22,8 +22,13 @@ const app = express();
 // Enable CORS
 app.use(
   cors({
-    origin: "https://poetic-tarsier-188c10.netlify.app",
-    credentials: true,
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:4173",
+      "http://127.0.0.1:3001",
+      process.env.PROD_FRONTEND_URL!,
+    ],
+    credentials: true, // optional if you send cookies or auth headers
   }),
 );
 
