a17738fa765a6979155eea500aee89f0f4e4bffa
knvcodes <knvcodes@gmail.com>
2026-06-29 17:27:34 +0530

    feat: added prod url for cors

--- src/server.ts
+++ src/server.ts
@@ -28,6 +28,7 @@ app.use(
       "http://localhost:3001",
       "http://localhost:4173",
       "http://127.0.0.1:3001",
+      !process.env.PROD_FRONTEND_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
